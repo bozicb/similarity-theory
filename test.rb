0f7d666b0b356a0628edfb2777d4f11683c74be0
Bojan <boj.bozic@gmail.com>
2021-03-14 17:03:58 +0000

    files moved

--- test.rb
+++ test.rb
@@ -5,19 +5,12 @@ athletes = Daru::DataFrame.new(
   {
     :Speed => [2.5,3.75,2.25,3.25,2.75,4.5,3.5,3.0,4.0,4.25,2.0,5.0,8.25,5.75,4.75,5.5,5.25,7.0,7.5,7.25],
     :Agility => [6.0,8.0,5.5,8.25,7.5,5.0,5.25,3.25,4.0,3.75,2.0,2.5,8.5,8.75,6.25,6.75,9.5,4.25,8.0,5.75],
-    :Draft => [false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true]
+    :Draft => [false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true]
   },
   index: (1..20).to_a
 )
 
-athletes.plot type: :scatter, x: 'Speed', y: 'Agility' do |plot, diagram|
-  plot.x_label 'Speed'
-  plot.y_label 'Agility'
-  plot.width 500
-  plot.height 400
-end
-
-prediction = knn(athletes,3,[6.75,3])
+prediction = KNN::knn(athletes,3,[6.75,3])
 puts "Prediction: #{prediction}"
 
-athletes.write_csv('athletes.csv')
+#athletes.write_csv('athletes.csv')
